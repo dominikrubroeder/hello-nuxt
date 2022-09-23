bddc5b94ef861c51528cdcdad1f722f3b4c06a13
Dominik <dominik.rubroeder@icloud.com>
2022-09-23 20:59:00 +0200

    Set up hello world project

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="flex items-center justify-center min-h-screen">
+    <h1 class="text-9xl">Hello, nuxt</h1>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
